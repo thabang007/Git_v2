9e4e16b97d4bbaf3df37e0b13ac01a5d579a97c3
thabang007 <thabang.mautjane@absa.africa>
2023-04-03 11:32:25 +0200

    print reversed function

--- printer.py
+++ printer.py
@@ -3,6 +3,9 @@ def print_header():
 def print_twice(input: str):
     str = input
     print(str*2)
+def print_reversed (input: str):
+    text = input[::-1]
+    print(text)
 if __name__ == "__main__":
     s = input()
     print_header()
