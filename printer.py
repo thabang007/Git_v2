1c1376bb57a50e716400b887b7e6c7daab7d2aa5
thabang007 <thabang.mautjane@absa.africa>
2023-04-03 12:07:37 +0200

    print header function

--- printer.py
+++ printer.py
@@ -1,4 +1,7 @@
+def print_header():
+      print("Thanks, now I'll print your string reversed, and twice")
 if __name__ == "__main__":
     s = input()
+    print_header()
     print_reversed(s)
     print_twice(s)
