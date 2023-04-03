0d7aecec7d3a7f2cf4b860ca2ba8a533e416ad14
thabang007 <thabang.mautjane@absa.africa>
2023-04-03 11:47:46 +0200

    print twice function Resolving conflicts

--- printer.py
+++ printer.py
@@ -1,5 +1,8 @@
 def print_header():
       print("Thanks, now I'll print your string reversed, and twice")
+def print_twice(input: str):
+    str = input
+    print(str*2)
 if __name__ == "__main__":
     s = input()
     print_header()
